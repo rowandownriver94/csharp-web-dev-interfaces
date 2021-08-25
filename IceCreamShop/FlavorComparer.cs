3bef6b6518ab95e4505177557774a64baab77362
Rowan Polster <rowandownriver94@gmail.com>
2021-08-25 01:05:26 -0500

    Finished steps 1-3, commented out bonus code - not sure how to implement it??

---
+++ IceCreamShop/FlavorComparer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IceCreamShop
+{
+    public class FlavorComparer : IComparer<Flavor>
+    {
+
+        public int Compare(Flavor x, Flavor y)
+        {
+            return string.Compare(x.Name, y.Name);
+        }
+        /*public int Compare(Flavor x, Flavor y)
+        {
+            if (x.Allergens.Count - y.Allergens.Count < 0)
+            {
+                return -1;
+            }
+            else if (x.Allergens.Count - y.Allergens.Count > 0)
+            {
+                return 1;
+            }
+            return 0;
+        }*/
+    }
+}
--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -11,13 +11,55 @@ namespace IceCreamShop
             List<Flavor> availableFlavors = menu.Flavors;
             List<Cone> availableCones = menu.Cones;
 
-        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+            // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
+            //  field.
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
+            Console.WriteLine("(before flavor sort)\n");
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+            foreach (Flavor flavor in availableFlavors)
+            {
+                Console.WriteLine(flavor.Name);
+            }
+
+            availableFlavors.Sort(new FlavorComparer());
+
+            // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
+            //  field.
+
+
+            // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
+            Console.WriteLine("\n\n(after flavor sort)\n");
+
+            foreach(Flavor flavor in availableFlavors)
+            {
+                Console.WriteLine(flavor.Name);
+            }
+
+
+
+            //ConeComparer 
+
+            Console.WriteLine("\n\n(before cone cost sort)\n");
+
+            foreach(Cone cone in availableCones)
+            {
+                Console.WriteLine($"{cone.Name}: {cone.Cost}");
+            }
+
+
+
+            availableCones.Sort(new ConeComparer());
+
+            Console.WriteLine("\n\n(after cone cost sort)\n");
+
+            foreach(Cone cone in availableCones)
+            {
+                Console.WriteLine($"{cone.Name}: {cone.Cost}");
+            }
+
+
+
         }
     }
 }
